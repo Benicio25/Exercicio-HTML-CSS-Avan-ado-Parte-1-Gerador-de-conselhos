c3ede6e519c1c793834257d52845a837906b1e91
Benicio <beniciojr18@gmail.com>
2025-08-10 14:17:24 -0300

    ultimos ajustes

--- src/js/script.js
+++ src/js/script.js
@@ -3,26 +3,25 @@ const adviceNumber = document.querySelector(".advice-id");
 const adviceDescription = document.querySelector(".advice-description");
 
 
-async function buscarConselho() {
-    try {
-        const response = await fetch("https://api.adviceslip.com/advice");
+async function searchAdvice() {
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
 
-        if (!response.ok) {
-            throw new Error("Erro ao buscar conselho");
-        }
-
-        const adviceContent = await response.json();
-        const adviceId = `advice #${adviceContent.slip.id}`;
-        const adviceText = `"${adviceContent.slip.advice}"`;
-
-        adviceNumber.innerText = adviceId;
-        adviceDescription.innerText = adviceText;
-
-    } catch (error) {
-        console.error("Erro ao buscar conselho:", error);
+    if (!response.ok) {
+      throw new Error("Erro ao buscar conselho");
     }
+
+    const adviceContent = await response.json();
+    const adviceId = `advice #${adviceContent.slip.id}`;
+    const adviceText = `"${adviceContent.slip.advice}"`;
+
+    adviceNumber.innerText = adviceId;
+    adviceDescription.innerText = adviceText;
+  } catch (error) {
+    console.error("Erro ao buscar conselho:", error);
+  }
 }
 
-adviceUpdateButton.addEventListener("click", buscarConselho);
+adviceUpdateButton.addEventListener("click", searchAdvice);
 
-buscarConselho();
+searchAdvice();
